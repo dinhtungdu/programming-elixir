d3446039d8d3ffc3737f445630f423999ec7212f
dinhtungdu <dinhtungdu@gmail.com>
2018-09-03 17:05:18 +0000

    Collection

--- list.exs
+++ list.exs
@@ -30,29 +30,25 @@ defmodule MyList do
   end
 
   def taxcalc(rates, orders) do
-    for order <- orders  do
-      if rates[order[:ship_to]] == nil do
-        order ++ [ total_amount: order[:net_amount] ]
-      else
-        order ++ [ total_amount: order[:net_amount] * (1 + rates[order[:ship_to]]) ]
-      end
-    end
+    # Enum.map( orders, fn x -> _taxcalc(x, rates) end) 
+    Enum.map( orders, _taxcalc(&1, rates) )
   end
 
-  defp _taxcalc(rates, order) do
+  defp _taxcalc(order = [id: _, ship_to: state, net_amount: net_amount], rates) do
+    tax_rate = Keyword.get(rates, state, 0)
+    total_amount = net_amount * (1 + tax_rate)
+    Keyword.put(order, :total_amount, total_amount)
   end
 end
 
-#orders = [
-        #[ id: 123, ship_to: :NC, net_amount: 100.00 ],
-        #[ id: 124, ship_to: :OK, net_amount:  35.50 ],
-        #[ id: 125, ship_to: :TX, net_amount:  24.00 ],
-        #[ id: 126, ship_to: :TX, net_amount:  44.80 ],
-        #[ id: 127, ship_to: :NC, net_amount:  25.00 ],
-        #[ id: 128, ship_to: :MA, net_amount:  10.00 ],
-        #[ id: 129, ship_to: :CA, net_amount: 102.00 ],
-        #[ id: 130, ship_to: :NC, net_amount:  50.00 ] ]
+orders = [
+        [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+        [ id: 124, ship_to: :OK, net_amount:  35.50 ],
+        [ id: 125, ship_to: :TX, net_amount:  24.00 ],
+        [ id: 126, ship_to: :TX, net_amount:  44.80 ],
+        [ id: 127, ship_to: :NC, net_amount:  25.00 ],
+        [ id: 128, ship_to: :MA, net_amount:  10.00 ],
+        [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+        [ id: 130, ship_to: :NC, net_amount:  50.00 ] ]
 
-#tax_rates = [ NC: 0.075, TX: 0.08 ]
-
-#IO.puts MyList.taxcalc(tax_rates, orders)
+tax_rates = [ NC: 0.075, TX: 0.08 ]
